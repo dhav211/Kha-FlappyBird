a4a4d92e9b4446833713b0b9fb90ddbf584c6239
dhav211 <dhav211@gmail.com>
2023-06-27 16:33:32 -0700

    Update Utils.hx no longer class now.

--- Sources/core/Utils.hx
+++ Sources/core/Utils.hx
@@ -1,11 +1,9 @@
 package core;
 
-class Utils {
-	static public function radToDeg(radians:Float):Float {
-		return radians * 180 / Math.PI;
-	}
+function radToDeg(radians:Float):Float {
+	return radians * 180 / Math.PI;
+}
 
-	static public function degToRad(degree:Float):Float {
-		return degree * Math.PI / 180;
-	}
+function degToRad(degree:Float):Float {
+	return degree * Math.PI / 180;
 }
